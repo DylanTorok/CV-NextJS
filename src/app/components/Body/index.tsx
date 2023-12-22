32668566ae993d7dce6c66c15f89ff44de81c430
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-22 16:03:26 +0100

    Ajout texte

--- src/app/components/Body/index.tsx
+++ src/app/components/Body/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import styles from './Body.module.css';
 
 const Body: React.FC = () => {
+
+  const classes = `${styles.body} ${styles.text}`
+
   return (
-    <h1 className={styles.h1}>Bonjour</h1>
+    <>
+    <p className={classes}>Vous trouverez ici mon parcours scolaire sous forme de frise chronologique, la liste de mes compétences techniques que j'ai acquises
+    à l'IUT ainsi que mes projets effectués lors des cours et ceux dans un cadre personnel.
+    Je vous souhaite une bonne visite !</p>
+    </>
   );
 };
 
