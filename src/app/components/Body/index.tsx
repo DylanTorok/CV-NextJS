739c7f9d30ae3799422a82b1da1ebb4ca835255c
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-22 11:49:25 +0100

    Début Body

---
+++ src/app/components/Body/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Body.module.css';
+
+const Body: React.FC = () => {
+  return (
+    <h1 className={styles.h1}>Bonjour</h1>
+  );
+};
+
+export default Body;
