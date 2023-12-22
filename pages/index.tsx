4fcca60e546aef51aee8cf5899a3a8f3635de0ac
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-22 16:02:23 +0100

    Ajout div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Navbar from '../src/app/components/Navbar';
 import Body from '../src/app/components/Body';
-import stylesNavbar from '../src/app/components/Navbar/Navbar.module.css';
+import styles from '../styles/index.module.css';
 
 const Home: React.FC = () => {
   const navigationLinks = {
     about: '/about',
-    skills: '/skills', 
+    skills: '/skills',
   };
 
   return (
-    <div className={stylesNavbar.background}>
-      <Navbar navigation={navigationLinks} />
-      <Body/>
+    <div className={styles.background}>
+      <div className={styles.center}>
+        <Navbar navigation={navigationLinks} />
+      </div>
+      <div className={styles.center}>
+        <Body />
+      </div>
     </div>
   );
 };
