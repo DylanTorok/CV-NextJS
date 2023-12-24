fab7b2cf44f3116087f078daa24d24107fde2f51
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-24 14:30:49 +0100

    Modification pour le footer en bas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../src/app/components/Navbar';
 import Body from '../src/app/components/Body';
+import Footer from '../src/app/components/Footer';
 import styles from '../styles/index.module.css';
 
 const Home: React.FC = () => {
@@ -11,9 +12,10 @@ const Home: React.FC = () => {
 
   return (
   <div>
-      <Navbar navigation={navigationLinks} />
+      <Navbar navigation={navigationLinks}/>
     <div className={styles.center}>
-      <Body />
+      <Body/>
+      <Footer/>
     </div>
   </div>
   );
