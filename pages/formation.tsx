4e278f1f7dc60b92ee910d99e7cf4e36b6e6bfb0
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-25 14:13:13 +0100

    page formation

--- pages/formation.tsx
+++ pages/formation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../src/app/staticComponents/Navbar';
-import Body from '../src/app/menu';
+import Body from '../src/app/formation';
 import Footer from '../src/app/staticComponents/Footer';
 import styles from '../styles/index.module.css';
 
@@ -8,6 +8,9 @@ const Formation: React.FC = () => {
   return (
     <div>
       <Navbar/>
+    <div className={styles.center}>
+      <Body/>
+    </div> 
     <Footer/>
 
   </div>
