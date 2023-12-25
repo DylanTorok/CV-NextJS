1bb3f987760a30e9d9e896accdef6749003aad90
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-25 17:43:07 +0100

    Page des compétences

--- pages/competences.tsx
+++ pages/competences.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../src/app/staticComponents/Navbar';
-import Body from '../src/app/menu';
+import Body from '../src/app/competences';
 import Footer from '../src/app/staticComponents/Footer';
 import styles from '../styles/index.module.css';
 
@@ -8,9 +8,11 @@ const Competences: React.FC = () => {
   return (
     <div>
       <Navbar/>
+      <div className={styles.center}>
+        <Body/>
+      </div> 
     <Footer/>
-
-  </div>
+    </div>
   );
 };
 
--- src/app/competences/index.tsx
+++ src/app/competences/index.tsx
@@ -1,18 +1,58 @@
+// Body.tsx
 import React from 'react';
 import styles from './Body.module.css';
 
+const languagesData = [
+  { name: 'Java', image: '/pictures/languages/Java.png', width: 80, height: 135, level: 75 },
+  { name: 'Python', image: '/pictures/languages/Python.png', width: 120, height: 135, level: 75 },
+  { name: 'SQL', image: '/pictures/languages/SQL.png', width: 275, height: 135, level: 60 },
+  { name: 'PHP', image: '/pictures/languages/PHP.png', width: 175, height: 135, level: 50 },
+  { name: 'TypeScript', image: '/pictures/languages/TypeScript.png', width: 135, height: 135, level: 50 },
+  { name: 'C', image: '/pictures/languages/C.png', width: 125, height: 135, level: 25 }
+];
+
+const softwaresData = [
+  { name: 'Eclipse', image: '/pictures/softwares/Eclipse.png', width: 135, height: 135, level: 80 },
+  { name: 'VS Code', image: '/pictures/softwares/VSCode.png', width: 135, height: 135, level: 80 },
+  { name: 'Git', image: '/pictures/softwares/Git.png', width: 135, height: 135, level: 60 },
+  { name: 'PhpStorm', image: '/pictures/softwares/PhpStorm.png', width: 135, height: 135, level: 60 },
+  { name: 'PyCharm', image: '/pictures/softwares/PyCharm.png', width: 135, height: 135, level: 60 },
+  { name: 'Docker', image: '/pictures/softwares/Docker.png', width: 200, height: 135, level: 50 }
+];
+
 const Body: React.FC = () => {
-
   return (
-    <ul className={styles.ul}>
-      <li className={styles.li}>{`Diplôme universitaire de trois ans en informatique | 2021 - Maintenant`}</li>
-      <p className={styles.p1}> {`IUT de Lens, Université d'Artois, 62300, France`}</p>
-
-      <li className={styles.li}> {`Baccalauréat | 2018 - 2021`}</li>
-      <p className={styles.p2}> {`Voie technologique (Sciences et technologies de l'industrie et du développement durable),
-      option SIN (Système d'information et numérique), Lycée Pasteur, Hénin-Beaumont, 62110, France`}
-      </p>
-    </ul>
+    <section className={styles.skillsSection}>
+      <h1 className={styles.h1}>{`Langages`}</h1>
+      <div className={styles.skillsContainer}>
+        {languagesData.map((language, index) => (
+          <div className={styles.skillItem} key={index}>
+            <img src={language.image} alt={language.name} width={language.width} height={language.height} />
+            <div className={styles.skillInfo}>
+              <p className={styles.p}>{language.name}</p>
+            </div>
+            <div className={styles.progressBarContainer}>
+              <div className={styles.progressBar} style={{ width: `${language.level}%` }}></div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr/>
+      <h1 className={styles.h1}>{`Logiciels`}</h1>
+      <div className={styles.skillsContainer}>
+        {softwaresData.map((software, index) => (
+          <div className={styles.skillItem} key={index}>
+            <img src={software.image} alt={software.name} width={software.width} height={software.height} />
+            <div className={styles.skillInfo}>
+              <p className={styles.p}>{software.name}</p>
+            </div>
+            <div className={styles.progressBarContainer}>
+              <div className={styles.progressBar} style={{ width: `${software.level}%` }}></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
