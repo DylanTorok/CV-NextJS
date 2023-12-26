2816f31f0ee85d083ccaabad58cd838eb6d6764a
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-26 15:20:52 +0100

    Page méteo et images

---
+++ pages/schoolProjects/weatherReport.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../../src/app/staticComponents/Navbar';
+import Body from '../../src/app/schoolProjects/weatherReport';
+import Footer from '../../src/app/staticComponents/Footer';
+import styles from '../../styles/index.module.css';
+
+const Fractals: React.FC = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div className={styles.center}>
+        <Body/>
+      </div> 
+      <Footer/>
+    </div>
+  );
+};
+
+export default Fractals;
