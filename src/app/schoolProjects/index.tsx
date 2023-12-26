5d539e791c2972248d79e843ab0e36f4c9c36686
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-26 15:28:40 +0100

    Correction bug route

--- src/app/schoolProjects/index.tsx
+++ src/app/schoolProjects/index.tsx
@@ -3,15 +3,15 @@ import styles from './Body.module.css';
 import Link from 'next/link';
 
 const projectsData = [
-  { name: 'Julia', image: '/pictures/schoolProjects/fractals/Julia.jpg', description: 'Génération de fractales en Java', width : 250, height : 250 },
-  { name: 'Meteo', image: '/pictures/schoolProjects/weatherReport/Welcome.jpg', description: "Création d'une API méteo",  width : 350, height : 200 }
+  { name: 'Julia', image: '/pictures/schoolProjects/fractals/Julia.jpg', description: 'Génération de fractales en Java', link : "/schoolProjects/fractals", width : 250, height : 250 },
+  { name: 'Meteo', image: '/pictures/schoolProjects/weatherReport/Welcome.jpg', description: "Création d'une API méteo", link : "/schoolProjects/weatherReport", width : 450, height : 200 }
 ];
 
 const Body: React.FC = () => {
   return (
     <section className={styles.body}>
       {projectsData.map((project, index) => (
-         <Link className={styles.link} href="/schoolProjects/weatherReport">
+         <Link className={styles.link} href={project.link}>
         <div className={styles.projectItem} key={index}>
           <img src={project.image} alt={project.name} width={project.width} height={project.height} />
           <div className={styles.projectInfo}>
