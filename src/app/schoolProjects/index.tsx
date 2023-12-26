137960c2172ece98be85849cba97157d02bc33db
dylan-torok <dylan_torok@ens.univ-artois.fr>
2023-12-26 15:20:23 +0100

    Ajout route vers méteo et changement de méthode pour les tailles des images

--- src/app/schoolProjects/index.tsx
+++ src/app/schoolProjects/index.tsx
@@ -3,16 +3,17 @@ import styles from './Body.module.css';
 import Link from 'next/link';
 
 const projectsData = [
-  { name: 'Julia', image: '/pictures/schoolProjects/fractals/Julia.jpg', description: 'Génération de fractales en Java' },
+  { name: 'Julia', image: '/pictures/schoolProjects/fractals/Julia.jpg', description: 'Génération de fractales en Java', width : 250, height : 250 },
+  { name: 'Meteo', image: '/pictures/schoolProjects/weatherReport/Welcome.jpg', description: "Création d'une API méteo",  width : 350, height : 200 }
 ];
 
 const Body: React.FC = () => {
   return (
     <section className={styles.body}>
       {projectsData.map((project, index) => (
-         <Link className={styles.link} href="/schoolProjects/fractals">
+         <Link className={styles.link} href="/schoolProjects/weatherReport">
         <div className={styles.projectItem} key={index}>
-          <img src={project.image} alt={project.name} className={styles.projectImage} />
+          <img src={project.image} alt={project.name} width={project.width} height={project.height} />
           <div className={styles.projectInfo}>
             <h2 className={styles.h2}>{project.name}</h2>
             <p className={styles.p}>{project.description}</p>
